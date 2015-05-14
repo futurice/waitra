22cf729471bfc07ee624fb73ff1eea201b448767
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-05-14 12:14:40 +0300

    Add example

--- test/Main.hs
+++ test/Main.hs
@@ -3,26 +3,25 @@ module Main (main) where
 
 import           Data.Aeson
 import           Data.String (fromString)
-import qualified Network.HTTP.Types as H
 import           Network.Wai
 import           Network.Wai.Test
 import           Network.Waitra
 import           Test.Tasty
+import           Network.HTTP.Types.Header
 import           Test.Tasty.HUnit
-import           Text.Regex.Applicative
 
 echoRoute :: Route
 echoRoute = routeGet (echoApp <$ string "/api/echo/" <*> many anySym)
-  where echoApp msg _req respond = respond $ responseLBS H.status200 [] (fromString msg)
+  where echoApp msg _req respond = respond $ responseLBS status200 [] (fromString msg)
 
 jsonRoute :: Route
 jsonRoute = simpleGet "/api/json" $ jsonApp (return . f)
-  where f (Object _) = (H.status200, [], "object" :: String)
-        f (Array _)  = (H.status200, [], "array")
-        f v          = (H.status404, [], show v)
+  where f (Object _) = (status200, [], "object" :: String)
+        f (Array _)  = (status200, [], "array")
+        f v          = (status404, [], show v)
 
 emptyApp :: Application
-emptyApp _ respond = respond $ responseLBS H.status404 [] (fromString "Fallback: Not found")
+emptyApp _ respond = respond $ responseLBS status404 [] (fromString "Fallback: Not found")
 
 app :: Application
 app = waitraMiddleware [echoRoute, jsonRoute] emptyApp
@@ -48,7 +47,7 @@ jsonCase1 = runApp $ do
   let req = setPath defaultRequest "/api/json"
   res <- srequest $ SRequest req "{}"
   assertStatus 200 res
-  assertHeader H.hContentType "application/json" res
+  assertHeader hContentType "application/json" res
   assertBody "\"object\"" res
 
 -- Cannot parse number as json
